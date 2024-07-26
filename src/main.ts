ccc6053a3b4001959985b41c2c0690798500cce8
Thiago Soares <tcsoares84@gmail.com>
2024-07-26 05:06:37 -0300

    fix: not required field

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get<ConfigService>(ConfigService);
   const port = configService.get('api.port');
--- src/schedule/dto/create-schedule.dto.ts
+++ src/schedule/dto/create-schedule.dto.ts
@@ -15,7 +15,7 @@ export class CreateScheduleDto {
 
   @IsString()
   @IsOptional()
-  finish: Date;
+  finish?: Date;
 
   @IsString()
   @IsOptional()
